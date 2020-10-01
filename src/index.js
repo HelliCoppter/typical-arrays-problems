ca0f3b92f8ee3173930d15e23e62ca39e3ce59d3
Alexander Fufachev <alexfufachev@mail.ru>
2020-10-02 02:21:27 +0300

    feat: done task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  let result = 0;
+  if (array !== undefined && array[0] !== undefined ) {
+    array.sort((a,b) => a - b);
+    result = array[0];
+  } 
+  return result;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let result = 0;
+  if ( array !== undefined && array[0] !== undefined ) {
+    array.sort((a,b) => a - b);
+    result = array[array.length - 1];
+  } 
+  return result;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let result = 0;
+  if (array !== undefined && array[0] !== undefined ) {
+    result = array.reduce((prev, current) => prev + current, 0) / array.length;
+  } 
+  return result;
 }
